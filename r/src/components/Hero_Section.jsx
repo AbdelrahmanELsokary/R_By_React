b12dd994a4f049566569448ab6725ae819437d79
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-19 01:29:58 +0200

    Edit: Font Size H1

--- r/src/components/Hero_Section.jsx
+++ r/src/components/Hero_Section.jsx
@@ -4,13 +4,13 @@ function Hero() {
       <section className=" bg-[#dee2e6]">
         <div className="container flex items-center justify-center gap-28 px-14">
           <div className="info">
-            <h1 className=" text-5xl font-mono font-extrabold leading-tight">
+            <h1 className=" sm:text-lg md:text-4xl lg:text-5xl font-mono font-extrabold leading-tight ">
               Hello
               <br />
               I’m Ridho
             </h1>
             <p>Freelancer Web Developer</p>
-            <a href="/" className="bg-[#212529] text-white block w-fit rounded-xl my-4 py-2 px-4">
+            <a href="/" className="bg-[#212529] text-white block w-24 text-center rounded-xl my-4 py-2 px-4">
               Hire Me
             </a>
           </div>
