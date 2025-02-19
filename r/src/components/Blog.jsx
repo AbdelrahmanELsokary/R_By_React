50b4d5aa62ef81bfdf2a9998a3fb0719ac492399
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-19 21:59:22 +0200

    Add: 4 Images To Images Folder

--- r/src/components/Blog.jsx
+++ r/src/components/Blog.jsx
@@ -1,4 +1,52 @@
 function Blog() {
-  return <></>;
+  return (
+    <>
+      <section className="blog" id="blog">
+        <div className="container py-5">
+          <h2 className=" text-center text-5xl font-bold">Blog</h2>
+          <div className="blog1 flex justify-center">
+            <div className="learning flex">
+              <div className="image"></div>
+              <div className="learn">
+                <h4>Learn HTML</h4>
+                <p>
+                  Lorem ipsum dolor <br /> sit amet, consecte adipiscing <br /> elit. Proin tristique elit nec. <br /> lorem ipsum
+                </p>
+              </div>
+            </div>
+            <div className="learning">
+              <div classNameName="image"></div>
+              <div className="learn">
+                <h4>Learn Css</h4>
+                <p>
+                  Lorem ipsum dolor <br /> sit amet, consecte adipiscing <br /> elit. Proin tristique elit nec. <br /> lorem ipsum
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="blog1 flex justify-center">
+            <div className="learning flex">
+              <div className="image"></div>
+              <div className="learn">
+                <h4>Learn Javascript</h4>
+                <p>
+                  Lorem ipsum dolor <br /> sit amet, consecte adipiscing <br /> elit. Proin tristique elit nec. <br /> lorem ipsum
+                </p>
+              </div>
+            </div>
+            <div className="learning">
+              <div className="image"></div>
+              <div className="learn">
+                <h4>Learn Go Lang</h4>
+                <p>
+                  Lorem ipsum dolor <br /> sit amet, consecte adipiscing <br /> elit. Proin tristique elit nec. <br /> lorem ipsum
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
-export default Blog
+export default Blog;
