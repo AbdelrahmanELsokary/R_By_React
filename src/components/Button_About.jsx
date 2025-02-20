53bfe99b7d69c669e9b16f28b0b66b9041014448
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-20 12:13:37 +0200

    Create: Layout Folder

--- src/components/Button_About.jsx
+++ src/components/Button_About.jsx
@@ -1,7 +1,9 @@
 function Button({ text }) {
   return (
     <>
-      <button className=" border-solid border-2 border-black w-24 text-center rounded-xl py-1.5 px-4 my-5 mx-1 last: hover:bg-[#212529] duration-500 hover:text-white hover:border-white">{text}</button>
+      <button className=" border-solid border-2 border-black w-24 text-center rounded-xl py-1.5 px-4 my-5 mx-1 last: hover:bg-[#212529] duration-500 hover:text-white hover:border-white">
+        {text}
+      </button>
     </>
   );
 }
