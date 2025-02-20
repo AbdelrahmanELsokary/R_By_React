de9de9bba421453a0a47edef16289d4c8e75e532
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-02-20 15:59:30 +0200

    Add: Drop Down To Navbar

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router";
+import { useState } from 'react';
+import { Link } from 'react-router';
 
 function Header() {
+  const [isOpenMenu, setMenu] = useState(false);
   return (
     <>
-      <header className="bg-[#212529] text-white py-1">
+      <header className="bg-[#212529] sticky top-0 text-white py-1">
         <div className="container w-4/5 px-6 m-auto flex justify-between items-center">
           <div className="logo">
             <a href="/" className="text-5xl">
@@ -11,24 +13,26 @@ function Header() {
             </a>
           </div>
           <nav>
-            <ul className="hidden md:flex gap-7">
-              <li>
+            <ul className={`${isOpenMenu ? 'absolute right-0 top-10 bg-[#212529] p-6 w-36' : 'hidden'} md:flex gap-7`}>
+              <li className="p-2 md:p-0">
                 <Link to="/">Home</Link>
               </li>
-              <li>
+              <li className="p-2 md:p-0">
                 <a href="#about">About</a>
               </li>
-              <li>
+              <li className="p-2 md:p-0">
                 <a href="#services">Services</a>
               </li>
-              <li>
+              <li className="p-2 md:p-0">
                 <a href="#blog">Blog</a>
               </li>
-              <li>
+              <li className="p-2 md:p-0">
                 <a href="#footer">Contact</a>
               </li>
             </ul>
-            <img src="/icons/menu.webp" alt="toggel icon for nav" className="md:hidden" />
+            <button className=" md:hidden" onClick={() => setMenu(!isOpenMenu)}>
+              <img src="/icons/menu.webp" alt="toggel icon for nav" className="md:hidden" />
+            </button>
           </nav>
         </div>
       </header>
